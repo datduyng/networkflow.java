afacc25e42c2549bb8e65e0166d21eae4b9cbf16
Reid Stagemeyer <reid.stagemeyer@gmail.com>
2019-03-27 11:16:19 -0500

    started hello world

--- src/com/networkflow/app/AppMain.java
+++ src/com/networkflow/app/AppMain.java
@@ -1,7 +1,11 @@
 package com.networkflow.app;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.settings.GameSettings;
+import com.sun.javafx.geom.Rectangle;
+import com.sun.prism.paint.Color;
 
 public class AppMain extends GameApplication {
 
@@ -9,11 +13,19 @@ public class AppMain extends GameApplication {
 	protected void initSettings(GameSettings settings) {
 		// TODO Auto-generated method stub
 		settings.setTitle("FXGL");
+		settings.setWidth(600);
+		settings.setHeight(600);
+		settings.setVersion("0.1");
 	}
+	
+	private Entity player;
 	
 	@Override
 	public void initGame() {
-		
+		 player = Entities.builder()
+				 .at(300, 300)
+                 //.viewFromNodeWithBBox(new Rectangle(25, 25, Color.BLUE))
+                 .buildAndAttach(getGameWorld());
 	}
 	
 	@Override
