8b964479cd214d32ec1091ff4e53ea9bfa9c134c
zoesophiey <zoesophiey@192.168.1.4>
2019-04-01 06:36:17 -0500

    start, stop, loadMap buttons

---
+++ src/JavafxDemoTest/JavafxDemo.java
@@ -0,0 +1,140 @@
+package JavafxDemoTest;
+
+import com.networkflow.component.JSONProcessor;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class JavafxDemo extends Application {
+	Button button;
+	private final Integer startSecond = 0;
+	private Integer seconds;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("test");
+
+		StackPane layout = new StackPane();
+
+
+		//start button
+		Button startBtn = new Button("Start button"); 
+		Button stopBtn = new Button("Stop button");
+
+		Label inputUpdateSpeed = new Label("timer update speed");
+		TextField input = new TextField();
+
+		Label l = new Label("button not selected");
+
+		//TODO:
+		//Double timerUpdateSpeed = Double.valueOf(input.getText());
+		Double timerUpdateSpeed = 1000.00;
+		
+		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) 
+			{ 		
+				Timeline timeline = new Timeline();
+				if(e.getSource() == startBtn){
+					seconds = startSecond;
+					l.setText("   start button   selected    "); 
+					System.out.println("Start Simulation");
+					timeline.setCycleCount(Timeline.INDEFINITE);
+					KeyFrame frame = new KeyFrame(Duration.millis(timerUpdateSpeed), new EventHandler<ActionEvent>() {
+						@Override
+						public void handle(ActionEvent event) {
+							// TODO Auto-generated method stub
+							seconds++;
+							System.out.println("current second: " +seconds);
+						} 
+					});
+					timeline.getKeyFrames().add(frame);
+					timeline.play();
+				}
+				else if(e.getSource() == stopBtn){
+					l.setText("   stop button   selected    "); 
+					System.out.println("Stop Simulation");
+					System.out.println("total time used: " +(seconds-startSecond));
+					seconds = 0;
+					timeline.stop();
+				}		
+			} 
+		}; 
+
+		startBtn.setOnAction(event);
+		stopBtn.setOnAction(event);
+
+
+		Button loadMapBtn = new Button("Load Map");
+		EventHandler<ActionEvent> eventLoadMap = new EventHandler<ActionEvent>() { 
+			public void handle(ActionEvent e) 
+			{ 
+				DirectoryChooser chooser = new DirectoryChooser();
+				chooser.setTitle("JavaFX Projects");
+				chooser.showDialog(primaryStage);
+				System.out.println("loading new map...");
+				System.out.println(chooser.showDialog(primaryStage).getAbsolutePath());
+			} 
+		}; 
+		loadMapBtn.setOnAction(eventLoadMap);
+
+
+		TilePane box2 = new TilePane();
+		box2.setPrefSize(160, 15);
+
+		box2.getChildren().addAll(inputUpdateSpeed, input);
+		box2.getChildren().add(startBtn);
+		box2.getChildren().add(stopBtn);
+		box2.getChildren().add(l);
+		box2.getChildren().add(loadMapBtn);
+		layout.getChildren().add(box2);
+
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+
+//	private void startTimer(){
+//		Timeline time = new Timeline();
+//		time.setCycleCount(Timeline.INDEFINITE);
+//		KeyFrame frame = new KeyFrame(Duration.millis(timerUpdateSpeed), new EventHandler<ActionEvent>() {
+//			@Override
+//			public void handle(ActionEvent event) {
+//				// TODO Auto-generated method stub
+//				seconds++;
+//				System.out.println("current second: " +seconds);
+//			} 
+//		});
+//		time.getKeyFrames().add(frame);
+//		time.play();
+//	}
+//	
+//	private void stoptimer(){
+//		
+//	}
+
+
+
+
+}
+
+//map button: start, stop, speed, load map
--- src/com/networkflow/app/ui/UserView.java
+++ src/com/networkflow/app/ui/UserView.java
@@ -8,19 +8,26 @@ import com.almasb.fxgl.ui.InGameWindow;
 import com.almasb.fxgl.ui.FXGLButton;
 import com.almasb.fxgl.ui.FXGLChoiceBox;
 
+import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.layout.VBox;
 
 
@@ -28,87 +35,152 @@ public class UserView extends InGameWindow{
 	public UserView(double width, double height) {
 		super("User View", WindowDecor.MINIMIZE);
 		
-		relocate(width - 202 - 202, height - 315);
+		//Stage primaryStage = new Stage();
 		
-        setBackgroundColor(Color.rgb(25, 25, 10, 0.4));
-        setPrefSize(202, 315);
-        setCanResize(false);
-        
-        VBox attrBox = new VBox(6);
-        attrBox.setSpacing(30); 
+		relocate(width - 202 - 202, height - 315);
 
-        
-        FXGLButton fooBtn = new FXGLButton("foo button");
-        EventHandler<ActionEvent> eventFoo = new EventHandler<ActionEvent>() { 
-            public void handle(ActionEvent e) 
-            { 
-                System.out.println("FX gL button");
-            } 
-        };
-        fooBtn.setOnAction(eventFoo);
-        Pane box = new Pane();
-        box.setPrefSize(160, 15);
-        box.getChildren().addAll(fooBtn);
-        attrBox.getChildren().add(box);
+		setBackgroundColor(Color.rgb(25, 25, 10, 0.4));
+		setPrefSize(202, 315);
+		setCanResize(false);
 
-        Button startBtn = new Button("Start button"); 
-        Label l = new Label("button not selected");
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
-            public void handle(ActionEvent e) 
-            { 
-                l.setText("  cancel  button    selected    "); 
-            } 
-        }; 
-        
-        startBtn.setOnAction(event);
-        Pane box2 = new Pane();
-        box2.setPrefSize(160, 15);
-        box2.getChildren().addAll(startBtn);
-        attrBox.getChildren().add(box2);
-        
-//        attrBox.setMargin(box, new Insets(50));
-//        attrBox.setMargin(box2, new Insets(50));
-        Font font = Font.font("Lucida Console", 14);
-        
-        Label opacityCaption = new Label("Simulation Speed");
-        opacityCaption.setFont(font);
-        Slider opacityLevel = new Slider(0, 1, 1);
-        Label opacityValue = new Label(
-                Double.toString(opacityLevel.getValue()));
-        opacityValue.setFont(font);
-        opacityLevel.valueProperty().addListener(new ChangeListener<Number>() {
-            public void changed(ObservableValue<? extends Number> ov,
-                Number old_val, Number new_val) {
-                    System.out.println("New Slider Values" + new_val.doubleValue());
-                    opacityValue.setText(String.format("%.2f", new_val));
-            }
-        });
+		VBox attrBox = new VBox(6);
+		attrBox.setSpacing(30); 
 
-        
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(10);
-        grid.setHgap(10);
-        
-        GridPane.setConstraints(opacityLevel, 0, 1);
-        grid.getChildren().add(opacityLevel);
-        
-        GridPane.setConstraints(opacityValue, 1, 1);
-        grid.getChildren().add(opacityValue);
-        
-        GridPane.setConstraints(opacityCaption, 0, 0);
-        grid.getChildren().add(opacityCaption);
-        
-        Pane box3 = new Pane();
-        box3.setPrefSize(160, 15);
-        box3.getChildren().addAll(grid);
-        attrBox.getChildren().add(box3);
-        
-        
-        
-        Pane pane = new Pane();
-        pane.getChildren().addAll(attrBox);
-        setContentPane(pane);
+		//foo button--------------------------------------
+		FXGLButton fooBtn = new FXGLButton("foo button");
+		EventHandler<ActionEvent> eventFoo = new EventHandler<ActionEvent>() { 
+			public void handle(ActionEvent e) 
+			{ 
+				System.out.println("FX gL button");
+			} 
+		};
+		fooBtn.setOnAction(eventFoo);
+		TilePane box = new TilePane();
+		box.setPrefSize(160, 15);
+		box.getChildren().addAll(fooBtn);
+		attrBox.getChildren().add(box);
+		
+		
+		//Input for updating timer speed button--------------------------------------
+		Label inputUpdateSpeed = new Label("timer update speed");
+		TextField input = new TextField();
+
+		//start and stop button--------------------------------------
+		Button startBtn = new Button("Start button"); 
+		
+		Button stopBtn = new Button("Stop button"); 
+		
+		Label l = new Label("button not selected");
+		
+		//TODO: syntax error 
+		Double timerUpdateSpeed = 500.00; //Double.valueOf(input.getText());
+		
+		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+			public void handle(ActionEvent e) 
+			{ 
+				Timeline timeline = new Timeline();
+				if(e.getSource() == startBtn){
+					l.setText("   start button   selected    "); 
+					Duration.millis(timerUpdateSpeed);
+					System.out.println("Start Simulation");
+					timeline.play();
+				}
+				else if(e.getSource() == stopBtn){
+					l.setText("   stop button   selected    "); 
+					timeline.stop();
+				}		
+			} 
+		}; 
+		startBtn.setOnAction(event);
+		stopBtn.setOnAction(event);
+		
+		TilePane box2 = new TilePane();
+		box2.setPrefSize(160, 15);
+		
+		box2.getChildren().addAll(inputUpdateSpeed, input);
+		box2.getChildren().add(startBtn);
+		box2.getChildren().add(stopBtn);
+		box2.getChildren().add(l);
+		
+		attrBox.getChildren().add(box2);
+
+
+
+		//load map button--------------------------------------
+		Button loadMapBtn = new Button("Load Map");
+		EventHandler<ActionEvent> eventLoadMap = new EventHandler<ActionEvent>() { 
+			public void handle(ActionEvent e) 
+			{ 
+				System.out.println("loading new map...");
+				DirectoryChooser chooser = new DirectoryChooser();
+				chooser.setTitle("JavaFX Projects");
+				//TODO: how to showDialog on scene
+				chooser.showDialog(primaryStage);
+				System.out.println(chooser.showDialog(primaryStage).getAbsolutePath());
+			} 
+		}; 
+		loadMapBtn.setOnAction(eventLoadMap);
+		TilePane boxLoadMap = new TilePane();
+		boxLoadMap.setPrefSize(160, 15);
+		boxLoadMap.getChildren().addAll(loadMapBtn);
+		attrBox.getChildren().add(boxLoadMap);
+
+		
+
+
+
+		//Simulation speed button--------------------------------------
+
+		//        attrBox.setMargin(box, new Insets(50));
+		//        attrBox.setMargin(box2, new Insets(50));
+		Font font = Font.font("Lucida Console", 14);
+
+		Label opacityCaption = new Label("Simulation Speed");
+		opacityCaption.setFont(font);
+		Slider opacityLevel = new Slider(0, 1, 1);
+		Label opacityValue = new Label(
+				Double.toString(opacityLevel.getValue()));
+		opacityValue.setFont(font);
+		opacityLevel.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue<? extends Number> ov,
+					Number old_val, Number new_val) {
+				System.out.println("New Slider Values" + new_val.doubleValue());
+				opacityValue.setText(String.format("%.2f", new_val));
+			}
+		});
+
+
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(10);
+		grid.setHgap(10);
+
+		GridPane.setConstraints(opacityLevel, 0, 1);
+		grid.getChildren().add(opacityLevel);
+
+		GridPane.setConstraints(opacityValue, 1, 1);
+		grid.getChildren().add(opacityValue);
+
+		GridPane.setConstraints(opacityCaption, 0, 0);
+		grid.getChildren().add(opacityCaption);
+
+		Pane box3 = new Pane();
+		box3.setPrefSize(160, 15);
+		box3.getChildren().addAll(grid);
+		attrBox.getChildren().add(box3);
+
+
+
+		Pane pane = new Pane();
+		pane.getChildren().addAll(attrBox);
+		setContentPane(pane);
+		
+//		Scene scene = new Scene(pane, width, height);
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
+		
+	
+		
 	}
 }
 
